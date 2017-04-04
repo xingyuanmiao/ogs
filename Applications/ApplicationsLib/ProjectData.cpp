c4eb19ed0c57a56f3d92d0c717f5b6f2b4220e37
xymiao <miaoxingyuan@live.cn>
2017-04-04 09:01:33 +0200

    disabled phasefield processes.

--- Applications/ApplicationsLib/ProjectData.cpp
+++ Applications/ApplicationsLib/ProjectData.cpp
@@ -41,8 +41,6 @@
 #include "ProcessLib/LIE/HydroMechanics/CreateHydroMechanicsProcess.h"
 #include "ProcessLib/LIE/SmallDeformation/CreateSmallDeformationProcess.h"
 #include "ProcessLib/LiquidFlow/CreateLiquidFlowProcess.h"
-#include "ProcessLib/PhaseField/CreatePhaseFieldProcess.h"
-#include "ProcessLib/TMPhaseField/CreateTMPhaseFieldProcess.h"
 #include "ProcessLib/ThermoMechanics/CreateThermoMechanicsProcess.h"
 #include "ProcessLib/RichardsFlow/CreateRichardsFlowProcess.h"
 #include "ProcessLib/SmallDeformation/CreateSmallDeformationProcess.h"
@@ -385,46 +383,6 @@ void ProjectData::parseProcesses(BaseLib::ConfigTree const& processes_config,
                 *_mesh_vec[0], std::move(jacobian_assembler),
                 _process_variables, _parameters, integration_order,
                 process_config);
-        }
-        else if (type == "PHASE_FIELD")
-        {
-            switch (process_config.getConfigParameter<int>("dimension"))
-            {
-                case 2:
-                    process =
-                        ProcessLib::PhaseField::createPhaseFieldProcess<
-                            2>(*_mesh_vec[0], std::move(jacobian_assembler),
-                               _process_variables, _parameters,
-                               integration_order, process_config);
-                    break;
-                case 3:
-                    process =
-                        ProcessLib::PhaseField::createPhaseFieldProcess<
-                            3>(*_mesh_vec[0], std::move(jacobian_assembler),
-                               _process_variables, _parameters,
-                               integration_order, process_config);
-                    break;
-            }
-        }
-        else if (type == "TMPHASE_FIELD")
-        {
-            switch (process_config.getConfigParameter<int>("dimension"))
-            {
-                case 2:
-                    process =
-                        ProcessLib::TMPhaseField::createTMPhaseFieldProcess<
-                            2>(*_mesh_vec[0], std::move(jacobian_assembler),
-                               _process_variables, _parameters,
-                               integration_order, process_config);
-                    break;
-                case 3:
-                    process =
-                        ProcessLib::TMPhaseField::createTMPhaseFieldProcess<
-                            3>(*_mesh_vec[0], std::move(jacobian_assembler),
-                               _process_variables, _parameters,
-                               integration_order, process_config);
-                    break;
-            }
         }
         else if (type == "THERMOMECHANICS")
         {
