edac32134736bc9efa14545ef62750ce04941935
Wenqing Wang <wenqing.wang@ufz.de>
2018-01-10 18:18:32 +0100

    [Name] Replaced pcs_idx and equation_id with process_id

--- ProcessLib/Process.cpp
+++ ProcessLib/Process.cpp
@@ -166,7 +166,7 @@ void Process::setInitialConditions(const int processs_id, double const t,
 }
 
 MathLib::MatrixSpecifications Process::getMatrixSpecifications(
-    const int /*equation_id*/) const
+    const int /*process_id*/) const
 {
     auto const& l = *_local_to_global_index_map;
     return {l.dofSizeWithoutGhosts(), l.dofSizeWithoutGhosts(),
